1bc081aa1978de5823fac7f5565c1615afe54a18
chen-zili <51958304+chen-zili@users.noreply.github.com>
2022-07-16 15:05:09 +0800

    Add files via upload

---
+++ digit2array.F90
@@ -0,0 +1,73 @@
+module BaseFunction
+    implicit none
+
+    ! 常量定义
+    integer(4), parameter :: companyNumber = 3
+    integer(4), parameter :: itemNumber = 5
+    integer(4), parameter :: judgeNumber = 7
+    integer(4), parameter :: judgeOneNumber = 6
+    integer(4), parameter :: oneCaseNumber = judgeOneNumber ** itemNumber
+    real(8)   , parameter :: allCaseNumber = dble(oneCaseNumber) ** dble(judgeNumber)
+    integer(4), parameter :: judgeOne(judgeOneNumber, companyNumber) = &
+                            (/0, 1, 2, &
+                              0, 2, 1, &
+                              1, 0, 2, &
+                              1, 2, 0, &
+                              2, 0, 1, &
+                              2, 1, 0/)
+
+    integer(4), parameter :: itemScores(itemNumber, companyNumber) = &
+                            (/25, 20, 15, &
+                            25, 20, 15, &
+                            10, 8, 6, &
+                            10, 8, 6, &
+                            30, 25, 20/)
+
+    contains
+
+        subroutine addOne(caseIndex, width, number)
+            implicit none
+            integer(4), intent(inout) :: caseIndex(width)
+            integer(4), intent(in) :: width
+            integer(4), intent(in) :: number
+            integer(4) :: i
+
+            do i = width, 1, -1
+                if (caseIndex(i) == number - 1) then
+                    caseIndex(i) = 0
+                else
+                    caseIndex(i) = caseIndex(i) + 1
+                    exit
+                end if
+            end do
+
+            return
+        end subroutine addOne
+
+        subroutine real2Array(caseIndex, width, number, value)
+            implicit none
+            integer(4), intent(inout) :: caseIndex(width)
+            integer(4), intent(in) :: width
+            integer(4), intent(in) :: number
+            real(8), intent(in) :: value
+            real(8) :: tmpMax
+            integer(4) :: i
+            
+            if (value < (dble(number) ** dble(width)) .and. value >= 0) then
+                caseIndex = 0
+
+                do i = 1, width
+                    tmpMax = dble(number) ** dble(width-i)
+                    if (value >= tmpMax) then
+                        caseIndex(i) = int(value / tmpMax)
+                        value = value - dble(caseIndex(i)) * tmpMax
+                    else if (dabs(value) <= 0.5d0) then
+                        exit
+                    end if
+                end do
+            end if
+
+            return
+        end subroutine
+
+end module BaseFunction

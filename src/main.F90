28f73d7e1d9dddb88eb4486a2a1011befac7fc8e
chen-zili <51958304+chen-zili@users.noreply.github.com>
2022-07-16 15:54:54 +0800

    Update main.F90

--- src/main.F90
+++ src/main.F90
@@ -9,8 +9,10 @@ program main
     ! 变量定义
     integer(4) :: caseJudgeIndex(judgeNumber) = 0       ! oneCaseNumber  ** judgeNumber
     integer(4) :: caseJudgeStopIndex(judgeNumber) = 0
-    integer(4) :: caseItemIndex(itemNumber)             ! judgeOneNumber ** itemNumber
+    integer(4) :: caseItemIndex(itemNumber) = 0         ! judgeOneNumber ** itemNumber
     integer(4) :: scoreTabOne(judgeNumber, itemNumber, companyNumber) = 0
+    integer(4) :: socreOne(companyNumber) = 0
+    integer(4) :: socreOneTmp(judgeNumber) = 0
 
 	integer(4) :: time_start, time_end
 	integer(4) :: rank = 0, size = 0, i, j, k, m, n
@@ -52,6 +54,40 @@ program main
     ! write(*, '(a, i, *(i))') 'Start', rank, caseJudgeIndex
     ! write(*, '(a, i, *(i))') 'Stop ', rank, caseJudgeStopIndex
 
+    do while (sum(caseJudgeStopIndex - caseJudgeIndex) >= 0)
+        ! 得分情况
+        do i = 1, judgeNumber
+            call int2Array(caseItemIndex, itemNumber, judgeOneNumber, caseJudgeIndex(i))
+            caseItemIndex = caseItemIndex + 1   ! 评分表是从1开始，计数器是从0开始
+
+            do j = 1, itemNumber
+                do k = 1, companyNumber
+                    scoreTabOne(i, j, k) = itemScores(j, judgeOne(caseItemIndex(j), k))
+                end do
+            end do
+        end do
+
+        ! 得分计算
+        socreOne = 0
+        do k = 1, companyNumber
+            do j = 1, itemNumber
+                socreOneTmp = scoreTabOne(1:judgeNumber, j, k)
+                socreOne(k) = socreOne(k) + sum(socreOneTmp) - max(socreOneTmp) - min(socreOneTmp)
+            end do
+        end do
+
+        ! 是否符合条件
+        if (socreOne(1) == socreOne(2) .and. socreOne(2) == socreOne(3)) then
+            counts = counts + 1
+            write(*, '(*(i))') caseJudgeIndex
+
+            open(20, file='./index.dat', position='APPEND')
+                read(20, '(*(i))') caseJudgeIndex
+            close(20)
+        end if
+        
+        call addOne(caseJudgeIndex, judgeNumber, oneCaseNumber)
+    end do
 
 	sync all											! 等待所有image运行完
 
@@ -72,4 +108,3 @@ program main
 
 	stop
 end program main
-
